3befe9d1149792be3b079e1557f00f2371aba2b9
piyachai kaewchum <kaonaka@hotmail.co.th>
2022-10-04 00:05:20 +0700

    :lipstick: update tailwind config - palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: 'class',
     content: ['./src/{pages,layouts,guards,components,sections}/**/*.{js,ts,jsx,tsx}'],
     theme: {
+        fontFamily: {
+            main: ['IBM Plex Sans Thai', 'sans-serif'],
+        },
         screens: {
             sm: '480px',
             md: '768px',
@@ -12,30 +16,35 @@ module.exports = {
             spacing: {
                 navbar: '70px',
             },
-            textColor: {},
+            textColor: {
+                DEFAULT: '#383838',
+                light: '#3838386b',
+                lighter: '#0000001a',
+            },
             boxShadow: {
                 card: '0px 7px 29px 0px rgba(100, 100, 111, 0.2) ',
             },
             colors: {
+                error: '#EF4444',
                 text: {
                     darkest: '#000',
                     darker: '#100000',
                     dark: '#202020',
-                    DEFAULT: '#404040',
-                    light: '#606060',
-                    lighter: '#707070',
+                    DEFAULT: '#383838',
+                    light: '#D1D0D0',
+                    lighter: '#0000001a',
                 },
                 paper: {
                     DEFAULT: '#fff',
-                    neutral: '#F8EDE3',
+                    neutral: '#fff',
                 },
                 primary: {
                     darkest: '#000',
                     darker: '#100000',
                     dark: '#202020',
-                    DEFAULT: '#404040',
-                    light: '#606060',
-                    lighter: '#707070',
+                    DEFAULT: '#F7931E',
+                    light: '#FFBB6B',
+                    lighter: '#FFDCB2',
                 },
                 secondary: {
                     darkest: '#000',
