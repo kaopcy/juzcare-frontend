a2b9f13fc72d1d10676f0b52ec05feec5caf2d84
piyachai kaewchum <kaonaka@hotmail.co.th>
2022-10-30 21:28:28 +0700

    :hammer: update home [hero]

--- src/sections/home/HomeHero.js
+++ src/sections/home/HomeHero.js
@@ -1,11 +1,17 @@
 import NextImage from 'next/image';
+import Logo from '@/svg/Logo';
 
 function HomeHero() {
    return (
       <section className="flex h-80 w-full items-start justify-center gap-x-16">
          <article className="mt-10 flex flex-col items-start">
             <h1 className="mb-3">ให้เราช่วยเหลือคุณสิ!</h1>
-            <p className="text-lg ">ปัญหาภายในมหาลัย สามารถแจ้งผ่าน</p>
+            <div className="flex items-end">
+               <p className="text-lg mr-2">ปัญหาภายในมหาลัย สามารถแจ้งผ่าน</p>
+               <div className="w-24 mb-1">
+                  <Logo/>
+               </div>
+            </div>
             <p className="text-lg mb-5">ให้เราเป็นคนรวบรวมปัญหาและแจ้งปัญหาแทนคุณเอง</p>
             <button className="text-md rounded-full bg-primary px-4 py-2 text-paper hover:bg-primary-dark">
                แจ้งปัญหา
