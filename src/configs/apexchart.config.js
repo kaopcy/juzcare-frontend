642a532fecc3b0a28f18d934a073518f98c65fb1
piyachai kaewchum <kaonaka@hotmail.co.th>
2022-10-31 20:06:54 +0700

    :hammer: update summary, add chart [apexchart]

---
+++ src/configs/apexchart.config.js
@@ -0,0 +1,61 @@
+const donutChartOption = {
+   legend: { show: false },
+   chart: {
+      animations: {
+         enabled: true,
+         easing: 'easeinout',
+         speed: 800,
+         dynamicAnimation: {
+            enabled: true,
+            speed: 350,
+         },
+      },
+      toolbar: { show: false },
+      zoom: { enabled: false },
+      foreColor: '#D9D9D9',
+      fontFamily: 'IBM Plex Sans Thai',
+   },
+   stroke: {
+      width: 3,
+      curve: 'smooth',
+      lineCap: 'round',
+   },
+   fill: {
+      type: 'gradient',
+      gradient: {
+         colorStops: [
+            { offset: 0, color: '#F7931E' },
+            { offset: 100, color: '#FFBB6B' },
+         ],
+      },
+   },
+   plotOptions: {
+      radialBar: {
+         track: {
+            strokeWidth: '100%',
+            background: '#e7e7e7',
+         },
+         dataLabels: {
+            name: { offsetY: -10 },
+            value: {
+               offsetY: 2,
+               color: '#383838',
+               fontSize: '22px',
+               fontWeight: '800',
+               lineHeight: '0px',
+            },
+            total: {
+               show: true,
+               label: 'ปัญหา',
+               color: '#ACACAC',
+               fontSize: '13px',
+               fontWeight: '700',
+               lineHeight: '0px',
+               formatter: () => 100,
+            },
+         },
+      },
+   },
+};
+
+export { donutChartOption };
