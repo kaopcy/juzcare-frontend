69358de11964c73ec4838eda7c7a0b0b9891d05d
piyachai kaewchum <kaonaka@hotmail.co.th>
2022-11-01 15:01:25 +0700

    :wrench: update start script

---
+++ src/contexts/ScrollObserverContext.js
@@ -0,0 +1,13 @@
+import { createContext } from 'react'
+
+const initialValue = {
+   scrollY: null,
+}
+
+const ScrollObserverContext = createContext(initialValue)
+
+export const ScrollObserverProvider = ({ children })=>{
+
+}
+
+
