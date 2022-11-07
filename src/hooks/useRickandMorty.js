0dfdc92a9f36ce3d9018c1d5081a1bade2f214cc
piyachai kaewchum <kaonaka@hotmail.co.th>
2022-11-07 21:16:22 +0700

    :recycle: refactor code

--- src/hooks/useRickandMorty.js
+++ src/hooks/useRickandMorty.js
@@ -3,48 +3,49 @@ import { useRouter } from 'next/router';
 
 // query
 import { gql, useQuery } from '@apollo/client';
+import client from '@/graphql/apollo-client';
 
 const ITEMS_PER_PAGE = 10;
 
 const QUERY = gql`
-    query CHARACTER($clientPage: Int) {
-        characters(page: $clientPage) {
-            info {
-                pages
-                count
-                prev
-                next
-            }
-            results {
-                name
-                id
-                image
-                status
-            }
-        }
-    }
+   query CHARACTER($clientPage: Int) {
+      characters(page: $clientPage) {
+         info {
+            pages
+            count
+            prev
+            next
+         }
+         results {
+            name
+            id
+            image
+            status
+         }
+      }
+   }
 `;
 
 export default function useRickandMorty(initPage) {
-    const [page, setPage] = useState(initPage);
-    const router = useRouter();
-    const { loading, data, error, fetchMore } = useQuery(QUERY, {
-        ssr: false,
-        variables: {
+   const [page, setPage] = useState(initPage);
+   const router = useRouter();
+   const { loading, data, error, fetchMore } = useQuery(QUERY, {
+      ssr: false,
+      variables: {
+         clientPage: page,
+      },
+   });
+
+   useEffect(() => {
+      if (loading || !page) return;
+      router.push(`/rickandmorty?page=${page}`, undefined, { shallow: true });
+      fetchMore({
+         variables: {
             clientPage: page,
-        },
-    });
+         },
+      });
+   }, [page, loading, fetchMore]);
 
-    useEffect(() => {
-        if (loading || !page) return;
-        router.push(`/rickandmorty?page=${page}`, undefined, { shallow: true });
-        fetchMore({
-            variables: {
-                clientPage: page,
-            },
-        });
-    }, [page, loading, fetchMore]);
-
-    const totalPage = useMemo(() => (loading ? null : data?.characters.info.pages), [data, loading]);
-    return { loading, rickandmorty: data, error, page, setPage, totalPage };
+   const totalPage = useMemo(() => (loading ? null : data?.characters.info.pages), [data, loading]);
+   return { loading, rickandmorty: data, error, page, setPage, totalPage };
 }
--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -3,95 +3,91 @@ import React from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
-
+// redux
+import { dispatch } from '@/redux/store';
+import { startSighIn } from '@/slices/user';
 // layout
 import Layout from '@/layouts/index';
-
 // components
 import InputText from '@/components/InputText';
 import GoogleLoginButton from '@/sections/login/GoogleLoginButton';
 import Background from '@/sections/login/BackgroundLog';
 import Logo from '@/sections/login/Logo';
 import IconGoogle from '@/sections/login/IconGoogle';
-
 // Path
 import Link from 'next/link';
 import { PATH } from '@/routes/index';
 
 const Login = () => {
-    const defaultValues = {
-        email: '',
-        password: '',
-    };
+   const defaultValues = {
+      email: '',
+      password: '',
+   };
 
-    const resolver = yup.object().shape({
-        email: yup.string().required('กรุณากรอกอีเมล์').email('กรุณาใช้อีเมล์ที่ถูกต้อง'),
-        password: yup.string().required('กรุณากรอกรหัสผ่าน').min(8, 'รหัสผ่านต้องเกิน 8 ตัว'),
-    });
+   const resolver = yup.object().shape({
+      email: yup.string().required('กรุณากรอกอีเมล์').email('กรุณาใช้อีเมล์ที่ถูกต้อง'),
+      password: yup.string().required('กรุณากรอกรหัสผ่าน').min(8, 'รหัสผ่านต้องเกิน 8 ตัว'),
+   });
 
-    const methods = useForm({
-        resolver: yupResolver(resolver),
-        defaultValues,
-    });
+   const methods = useForm({
+      resolver: yupResolver(resolver),
+      defaultValues,
+   });
 
-    const onSubmit = (value) => {
-        console.log(value);
-    };
+   const onSubmit = (value) => {
+      dispatch(startSighIn({ value }));
+      console.log(value);
+   };
 
-    const { formState } = methods;
+   const { formState } = methods;
 
-    return (
-        <div className="flex items-center justify-center w-full h-screen scale-90 bg-paper-neutral">
-            <div className="relative flex items-end ml-32">
-                <section className="absolute z-10 flex flex-col p-10 rounded-md bg-paper-neutral top-5">
-                    <Logo className="w-[300px] h-full mx-auto " />
-                    <h1 className="self-center my-6 text-2xl">เข้าสู่ระบบ</h1>
-                    <FormProvider {...methods}>
-                        <form onSubmit={methods.handleSubmit(onSubmit)}>
-                            <section className="flex flex-col items-center justify-center gap-y-6 w-[350px] ">
-                                <InputText label="อีเมล์" name="email" />
-                                <InputText label="รหัสผ่าน" name="password" type="password" />
+   return (
+      <div className="flex h-screen w-full scale-90 items-center justify-center bg-paper-neutral">
+         <div className="relative ml-32 flex items-end">
+            <section className="absolute top-5 z-10 flex flex-col rounded-md bg-paper-neutral p-10">
+               <Logo className="mx-auto h-full w-[300px] " />
+               <h1 className="my-6 self-center text-2xl">เข้าสู่ระบบ</h1>
+               <FormProvider {...methods}>
+                  <form onSubmit={methods.handleSubmit(onSubmit)}>
+                     <section className="flex w-[350px] flex-col items-center justify-center gap-y-6 ">
+                        <InputText label="อีเมล์" name="email" />
+                        <InputText label="รหัสผ่าน" name="password" type="password" />
 
-                                <button
-                                    className="w-full py-2.5 my-3 text-base rounded-sm bg-primary 
-                                    text-paper drop-shadow-md hover:bg-paper-neutral hover:text-primary 
-                                    border-solid border-2 border-primary
+                        <button
+                           className="my-3 w-full rounded-sm border-2 border-solid border-primary 
+                                    bg-primary py-2.5 text-base text-paper 
+                                    drop-shadow-md hover:bg-paper-neutral hover:text-primary
                                     active:border-primary"
-                                    type="submit"
-                                >
-                                    เข้าสู่ระบบ
-                                </button>
-                            </section>
-                        </form>
+                           type="submit"
+                        >
+                           เข้าสู่ระบบ
+                        </button>
+                     </section>
+                  </form>
 
-                        <div className="flex flex-col items-center justify-between w-full my-6 gap-y-6">
-        
-                            <GoogleLoginButton />
-                         
-                      
-                            <Link href={PATH.auth.register}>
-                                <button
-                                    className="font-normal text-black underline text-md hover:text-primary"
-                                    type="button"
-                                >
-                                    สมัครใช้งาน
-                                </button>
-                            </Link>
-                        </div>
-                    </FormProvider>
-                </section>
-                <div className="w-[1260px] h-full opacity-70">
-                    <Background />
-                </div>
+                  <div className="my-6 flex w-full flex-col items-center justify-between gap-y-6">
+                     <GoogleLoginButton />
+
+                     <Link href={PATH.auth.register}>
+                        <button className="text-md font-normal text-black underline hover:text-primary" type="button">
+                           สมัครใช้งาน
+                        </button>
+                     </Link>
+                  </div>
+               </FormProvider>
+            </section>
+            <div className="h-full w-[1260px] opacity-70">
+               <Background />
             </div>
-        </div>
-    );
+         </div>
+      </div>
+   );
 };
 
 Login.getLayout = (page) => (
-    <Layout title="เข้าสู่ระบบ" variant="unauthenticated">
-        {page}
-    </Layout>
+   <Layout title="เข้าสู่ระบบ" variant="auth">
+      {page}
+   </Layout>
 );
 
 export default Login;
