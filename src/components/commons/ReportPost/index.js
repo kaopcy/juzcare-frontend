e8d5cb5c710e9183d7feb977893a08df0ab01e8f
piyachai kaewchum <kaonaka@hotmail.co.th>
2022-11-10 00:45:23 +0700

    :hammer: update [_mock, reportContext]

--- src/components/commons/ReportPost/index.js
+++ src/components/commons/ReportPost/index.js
@@ -1,21 +1,28 @@
+import PropTypes from 'prop-types';
 import { useMemo } from 'react';
 // components
 import Image from 'next/image';
 import ReportPostHeader from './ReportPostHeader';
 import ReportPostImageSlider from './ReportPostImageSlider';
 import Icon from '@/components/Icon';
+// contexts
+import { ReportContextProvider } from '@/contexts/reports/ReportContext';
 
-function ReportPost() {
+ReportPost.propTypes = {
+   report: PropTypes.object,
+};
+
+function ReportPost({ report }) {
    return (
-      <>
+      <ReportContextProvider initVal={report}>
          <article className="flex w-full text-text">
             <div className="relative mr-3 h-16 w-16 shrink-0 overflow-hidden rounded-full">
-               <Image alt="bird" src="https://source.unsplash.com/aeNg4YA41P8" objectFit="cover" layout="fill" />
+               <Image alt="bird" src={report.user.avatar.avatarUrl} objectFit="cover" layout="fill" />
             </div>
             <div className="flex w-full flex-col gap-y-5">
                <ReportPostHeader />
                <ReportPostImageSlider />
-               <p>น้ำในห้องน้ำตึกโหลไม่ไหลเลยอะ เป็นแบบนี้มาหลายอาทิตย์แล้วอะ อารมณ์เสียเลย ปวดขี้ก็ปวด ทำไรไม่ได้</p>
+               <p>{report.detail}</p>
                <div className="flex text-text-light">
                   <button className="flex items-center">
                      <Icon className="mr-1 h-5 w-5" icon="ant-design:like-outlined" />
@@ -29,7 +36,7 @@ function ReportPost() {
             </div>
          </article>
          <div className="h-[1px] w-full bg-text-lighter " />
-      </>
+      </ReportContextProvider>
    );
 }
 
--- src/pages/reports/index.js
+++ src/pages/reports/index.js
@@ -1,28 +1,39 @@
+import PropTypes from 'prop-types';
 import Layout from '@/layouts/index';
 // components
 import ReportPost from '@/components/commons/ReportPost';
 // sections
 import ReportsTypesSelector from '@/sections/reports/ReportsTypesSelector';
 import ReportsOptions from '@/sections/reports/ReportsOptions';
+// services
+import { getPosts } from '@/services/reports.service';
 
-function Reports() {
+Reports.propTypes = {
+   reports: PropTypes.object,
+};
+
+function Reports({ reports }) {
    return (
       <section className="relative flex  w-full justify-center">
-         <div className="flex w-full mx-[10%]  max-w-[700px] flex-col gap-y-10 py-12">
+         <div className="mx-[10%] flex w-full  max-w-[700px] flex-col gap-y-10 py-12">
             <ReportsTypesSelector />
-            <ReportPost />
-            <ReportPost />
-            <ReportPost />
-            <ReportPost />
+            {reports.map((report) => (
+               <ReportPost key={report._id} report={report} />
+            ))}
          </div>
          <ReportsOptions />
       </section>
    );
 }
 
-export const getServersideProps = async () => {
+export const getServerSideProps = async () => {
+   const reports = await getPosts({
+      sort: 'something',
+   });
    return {
-      props: {},
+      props: {
+         ...reports,
+      },
    };
 };
 
